04af136a0f9fe744c34bee764d51d6454c20a0f2
Kenny Grage <kennygrage@gmail.com>
2015-09-30 15:02:51 -0700

    start making deeds

--- community_chest_and_chance_card_data.js
+++ community_chest_and_chance_card_data.js
@@ -1,24 +1,35 @@
 var community_chest_data = [];
 var chance_data = [];
 
+//******************OBJECT VALUES******************//
+//text: words on card displayed with a bigger font size
+//subtext: words on card displayed with a smaller font size
+//kind: "card"   - if the object, value, is zero, then the player is retaining the card
+//               - if the object, value, is not zero, then the player will collect (or pay if value is negative) each player the amount of value
+//      "money"  - the player will collect the amount of value (or pay if value is negative)
+//      "go"     - the player will move to a certain spot of the board. If value is negative, then player will move back those spaces, if value is positive, then player will move to that position on the board(i.e. position 0 is "Go").
+//      "assess" - the player will have to pay the amount of value, passed in as negative numbers, the amount of the first element in value per house owned plus the amount of the second element in value per hotel owned.
+//value: value can equal the amount of money player needs to pay or receive, the board position the player is to move to, etc, depending on the value of the object, kind.
+
 //Community Chest
-community_chest_data[0] = {text: "ADVANCE TO GO", subtext: "(COLLECT $200)", move: "go", value: 200}; //Advance to Go (Collect $200)
-community_chest_data[1] = ""; //Bank error in your favor – collect $200
-community_chest_data[2] = {text: "PORTLAND ARTS TAX" subtext: "PAY $50"}; //Doctor's fees – Pay $50
-community_chest_data[3] = {text: "GET OUT OF PORTLAND SATURDAY MARKET, FEE", subtext: "THIS CARD MAY BE KEPT UNTIL NEEDED OR SOLD"}; //Get out of jail free – this card may be kept until needed, or sold
-community_chest_data[4] = {text: "GO TO PORTLAND SATURDAY MARKET", subtext: "GO DIRECT TO PORTLAND SATURDAY MARKET - DO NOT PASS GO - DO NOT COLLECT $200"}; //Go to jail – go directly to jail – Do not pass Go, do not collect $200
-community_chest_data[5] = "Plan an Event at Oaks Amusement Park - Collect $10 from each player"; //It is your birthday Collect $10 from each player
-community_chest_data[6] = {text: "WIN DANCE CONTEST AT THE CRYSTAL BALLROOM", subtext: "COLLECT $50 FROM EACH PLAYER"; //Grand Opera Night – collect $50 from every player for opening night seats
-community_chest_data[7] = ""; //Income Tax refund – collect $20
-community_chest_data[8] = ""; //Life Insurance Matures – collect $100
-community_chest_data[9] = {text: "TOUR PORTLAND'S CRAFT BREWERIES", subtext: "PAY $100 FOR BEER"; //Pay Hospital $100
-community_chest_data[10] = "Visit Portland Art Museum - Pay $50"; //Pay School Fees of $50
-community_chest_data[11] = {text: "GATHER SIGNATURES AT TOM MCCALL WATERFRONT PARK", subtext: "COLLECT $25"}; //Receive for services $25
-community_chest_data[12] = "Portland's School Bond Passes! Pay $40 per house, $115 per hotel"; //You are assessed for street repairs – $40 per house, $115 per hotel
-community_chest_data[13] = "You visit the Paul Bunyan Statue and find someone's lunch money - Collect $10"; //You have won second prize in a beauty contest– collect $10
-community_chest_data[14] = {text: "SELL YOUR FAMILY PASS TO THE OREGON ZOO", subtext: "COLLECT $100"}; //You inherit $100
-community_chest_data[15] = {text: "WIN A SOCCERT TOURNAMENT AT DELTA PARK", subtext: "COLLECT $45"}; //From sale of stock you get $45
-community_chest_data[16] = ""; //Holiday Fund matures - Receive $100
+community_chest_data[0] = {text: "WIN DANCE CONTEST AT THE CRYSTAL BALLROOM", subtext: "COLLECT $50 FROM EACH PLAYER", kind: "card", value: [50]}; //when kind === card, if value is not zero, then we are paying or collecting money from each player-------Grand Opera Night – collect $50 from every player for opening night seats
+community_chest_data[1] = {text: "GATHER SIGNATURES AT TOM MCCALL WATERFRONT PARK", subtext: "COLLECT $25", kind: "money", value: [25]}; //Receive for services $25
+community_chest_data[2] = {text: "ADVANCE TO GO", subtext: "(COLLECT $200)", kind: "go", value: [0]}; //Advance to Go (Collect $200)
+community_chest_data[3] = {text: "TOUR PORTLAND'S CRAFT BREWERIES", subtext: "PAY $100 FOR BEER", kind: "money", value[-100]}; //Pay Hospital $100
+community_chest_data[4] = {text: "PORTLAND ARTS TAX" subtext: "PAY $50", kind: "money", value[-50]}; //Doctor's fees – Pay $50
+community_chest_data[5] = {text: "GET OUT OF PORTLAND SATURDAY MARKET, FREE", subtext: "THIS CARD MAY BE KEPT UNTIL NEEDED OR SOLD", kind: "card", value: [0]}; //Get out of jail free – this card may be kept until needed, or sold
+community_chest_data[6] = {text: "WIN A SOCCERT TOURNAMENT AT DELTA PARK", subtext: "COLLECT $45", kind: "money", value[45]}; //From sale of stock you get $45
+community_chest_data[7] = {text: "SELL YOUR FAMILY PASS TO THE OREGON ZOO", subtext: "COLLECT $100", kind: "money", value[100]}; //You inherit $100
+community_chest_data[8] = {text: "GO TO PORTLAND SATURDAY MARKET", subtext: "GO DIRECT TO PORTLAND SATURDAY MARKET - DO NOT PASS GO - DO NOT COLLECT $200", kind: "go", value: [10]}; //Go to jail – go directly to jail – Do not pass Go, do not collect $200
+community_chest_data[9] = {text: "BRING OUT OF TOWN FAMILY TO MULTNOMAH FALLS", subtext: "COLLECT $100", kind: "money", value[100]}; //Life Insurance Matures – collect $100
+community_chest_data[10] = {text: "VISIT THE PAUL BUNYAN STATUE AND FIND SOMEONE'S LUNCH MONEY", subtext: "COLLECT $10", kind: "money", value[10]}; //You have won second prize in a beauty contest– collect $10
+community_chest_data[11] = {text: "WIN THE PORTLAND MARATHON", subtext: "COLLECT $100", kind: "money", value[100]}; //Xmas Fund matures - Receive $100
+community_chest_data[12] = {text: "PORTLAND'S SCHOOL BOND PASSES!", subtext: "PAY $40 PER HOUSE, $115 PER HOTEL", kind: "assess", value[-40, -115]}; //You are assessed for street repairs – $40 per house, $115 per hotel
+community_chest_data[13] = {text: "", subtext: "", kind: "money", value[200]}; //Bank error in your favor – collect $200
+community_chest_data[14] = {text: "FIND HAIR IN YOUR FOOD CART LUNCH", subtext: "COLLECT $20", kind: "money", value[20]}; //Income Tax refund – collect $20
+community_chest_data[15] = {text: "PLAN AN EVENT AT OAKS AMUSEMENT PARK", subtext: "COLLECT $10 FROM EACH PLAYER", kind: "card", value[10]}; //when kind === card, if value is not zero, then we are paying or collecting money from each player-------It is your birthday Collect $10 from each player
+community_chest_data[16] = {text: "VISIT PORTLAND ART MUSEUM", subtext: "PAY $50", kind: "money", value: [50]}; //Pay School Fees of $50
+
 
 //Chance
 chance_data[0] = "Advance to Go (Collect $200)"; //Advance to Go (Collect $200)
@@ -46,11 +57,11 @@ HOLD
 
 move:
 go -
-market - type: "go", value: [10]
-get out of market free: type: "card"
+market - kind: "go", value: [10]
+get out of market free: kind: "card"
 illinois
-st. charles - type: "go", value: [11]
-utility - type: "go", value: [12, 28]
-railroad - type: "go", value: [5, 15, 25, 35]
-bank pays $50 - type: "money", value: [50]
-go back three spaces: type: "go", value: [-3]
+st. charles - kind: "go", value: [11]
+utility - kind: "go", value: [12, 28]
+railroad - kind: "go", value: [5, 15, 25, 35]
+bank pays $50 - kind: "money", value: [50]
+go back three spaces: kind: "go", value: [-3]
---
+++ deeds.js
@@ -0,0 +1,369 @@
+var deeds = [{name: "Metal Toad", price: 60, group_id: 1, deeds_in_group, owned: 0, id: 1},
+            {name: "New Relic", price: 80, group_id: 1, owned: 2, id: 2},
+
+
+              ];
+
+
+
+var deeds[0] = {name: "Go",
+                price: 0,
+                mortgage_value: 0,
+                house_cost: 0,
+                rent: [],
+                group_id: 0,
+                owned: 0
+                };
+
+//Mediterrian Ave
+var deeds[1] = {name: "",
+                price: 60,
+                mortgage_value: 30,
+                house_cost: 50,
+                rent: [2, 10, 30, 90, 160],
+                group_id: 0,
+                owned: 0
+                };
+
+//Community Chest
+var deeds[2] = {name: "",
+                price: ,
+                mortgage_value: ,
+                house_cost: ,
+                rent: [],
+                group_id: ,
+                owned: 0
+                };
+
+var deeds[3] = {name: "",
+                price: ,
+                mortgage_value: ,
+                house_cost: ,
+                rent: [],
+                group_id: ,
+                owned: 0
+                };
+
+var deeds[4] = {name: "",
+                price: ,
+                mortgage_value: ,
+                house_cost: ,
+                rent: [],
+                group_id: ,
+                owned: 0
+                };
+
+var deeds[] = {name: "",
+               price: ,
+               mortgage_value: ,
+               house_cost: ,
+               rent: [],
+               group_id: ,
+               owned: 0
+               };
+
+var deeds[] = {name: "",
+               price: ,
+               mortgage_value: ,
+               house_cost: ,
+               rent: [],
+               group_id: ,
+               owned: 0
+               };
+
+var deeds[] = {name: "",
+               price: ,
+               mortgage_value: ,
+               house_cost: ,
+               rent: [],
+               group_id: ,
+               owned: 0
+               };
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+price: ,
+mortgage_value: ,
+house_cost: ,
+rent: [],
+group_id: ,
+owned: 0
+};
+
+var deeds[] = {name: "",
+            price: ,
+            mortgage_value: ,
+            house_cost: ,
+            rent: [],
+            group_id: ,
+            owned: 0
+            };
