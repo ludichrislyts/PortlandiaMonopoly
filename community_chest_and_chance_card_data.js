c2eae320da6866caecff4ef026c6258fd1dc2cae
Kenny Grage <kennygrage@gmail.com>
2015-09-30 10:32:18 -0700

    add community chest and chance arrays without data

---
+++ community_chest_and_chance_card_data.js
@@ -0,0 +1,40 @@
+var community_chest_data = [];
+var chance_data = [];
+
+//Community Chest
+community_chest_data[0] = "";
+community_chest_data[1] = "";
+community_chest_data[2] = "";
+community_chest_data[3] = "";
+community_chest_data[4] = "";
+community_chest_data[5] = "";
+community_chest_data[6] = "";
+community_chest_data[7] = "";
+community_chest_data[8] = "";
+community_chest_data[9] = "";
+community_chest_data[10] = "";
+community_chest_data[11] = "";
+community_chest_data[12] = "";
+community_chest_data[13] = "";
+community_chest_data[14] = "";
+community_chest_data[15] = "";
+community_chest_data[16] = "";
+
+//Chance
+chance_data[0] = "";
+chance_data[1] = "";
+chance_data[2] = "";
+chance_data[3] = "";
+chance_data[4] = "";
+chance_data[5] = "";
+chance_data[6] = "";
+chance_data[7] = "";
+chance_data[8] = "";
+chance_data[9] = "";
+chance_data[10] = "";
+chance_data[11] = "";
+chance_data[12] = "";
+chance_data[13] = "";
+chance_data[14] = "";
+chance_data[15] = "";
+chance_data[16] = "";
