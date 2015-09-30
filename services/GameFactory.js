ac97a1c8c94045bc04c9ee1ff0eb74bb49aebb19
Chris <ludichrislyts@gmail.com>
2015-09-30 11:35:26 -0700

    pull from project master

--- services/GameFactory.js
+++ services/GameFactory.js
@@ -3,7 +3,10 @@ portlandiaMonopoly.factory('GameFactory', function GameFactory(){
   // elements of the game
   factory.players = [];
   factory.properties = [];
-  factory.actionCards = [];
+  // contains 2 arrays -- chance and community chest
+  //CHANCE CARDS = 0
+  //COMMUNITY CHEST = 1
+  factory.actionCards = [[chance_cards], [community_chest_cards]];
 
   factory.addPlayer = function(){
     factory.players.push({ id: factory.players.length + 1,
@@ -13,6 +16,10 @@ portlandiaMonopoly.factory('GameFactory', function GameFactory(){
                          });
      factory.playerName = null;
   };
+
+  factory.addChanceCard(){
+    
+  }
 
 
 
