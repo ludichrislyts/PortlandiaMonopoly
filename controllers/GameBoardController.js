d3ec2eece916ecd0280d18821de7f6e8214fd163
JackMackerel <ludichrislyts@gmail.com>
2015-10-03 21:11:12 -0700

    commit before branch

--- controllers/GameBoardController.js
+++ controllers/GameBoardController.js
@@ -5,15 +5,16 @@ portlandiaMonopoly.controller('GameBoardCtrl', function GameBoardCtrl($scope, $s
 	$scope.deeds = DeedsFactory.deeds;
 	$scope.players = GameFactory.players;
 	$scope.pieces = GameFactory.gamePieces;
+	$scope.startGame = false;
 	
 	$scope.playerPiece = GameFactory.players.playerPiece;
+	$scope.update = function(){
+		$scope.remainingPieces = GameFactory.remainingGamePieces;
+		$scope.factory.playerPiece = GameFactory.remainingGamePieces[0];
+	};
 	$scope.remainingPieces = GameFactory.remainingGamePieces;
 	// initial startGame value to be false. Toggled true in partial when user selects
 	// to start game
-	$scope.startGame = false;
 	
 	// changes the selected piece in the drop down to reflect already taken pieces
-	$scope.update = function(){
-		$scope.factory.playerPiece = GameFactory.remainingGamePieces[0];
-	};
 });
