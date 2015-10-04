2d32395b06f76541641bcf915f610ed7e8387bbb
JackMackerel <ludichrislyts@gmail.com>
2015-10-04 01:01:14 -0700

    move dice.js to DiceFactory in services folder and get rollForFirst function to work but not display to screen

--- controllers/PlayerSelectController.js
+++ controllers/PlayerSelectController.js
@@ -1,22 +1,59 @@
 portlandiaMonopoly.controller('PlayerSelectCtrl', function PlayerSelectCtrl($scope, $stateParams, GameFactory, UtilitiesFactory) {
 	
 	$scope.factory = GameFactory;
-	
 	// $scope.deeds = DeedsFactory.deeds;
-	$scope.players = GameFactory.players;
-	$scope.pieces = GameFactory.gamePieces;
-	$scope.startGame = false;
 	
-	// TESTING ONLY //
+	// TESTING ONLY!! Comment out and un-comment next $scope.players line when game is live
 	$scope.players = GameFactory.seedPlayerArray();
-	// $scope.playerPiece = GameFactory.players.playerPiece;
+	// $scope.players = GameFactory.players;
+	$scope.pieces = GameFactory.gamePieces;
+	// initial startGame value to be false. Toggled true in partial when user selects
+	// to start game
+	$scope.startGame = false;	
+/////////////////////////////////////////////////////////////////////
+/////////////////// select player controls //////////////////////////
+/////////////////////////////////////////////////////////////////////
 	$scope.update = function(){
 		$scope.remainingPieces = GameFactory.remainingGamePieces;
+	// changes the selected piece in the drop down to show top available piese
 		$scope.factory.playerPiece = GameFactory.remainingGamePieces[0];
 	};
 	$scope.remainingPieces = GameFactory.remainingGamePieces;
-	// initial startGame value to be false. Toggled true in partial when user selects
-	// to start game
-	
-	// changes the selected piece in the drop down to reflect already taken pieces
+/////////////////////////////////////////////////////////////////////
+/////////////////// end select player controls //////////////////////
+/////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////
+/////////////////// highest roller controls /////////////////////////
+/////////////////////////////////////////////////////////////////////
+	var highestRoller = [{player: null, roll: 0, show: false}];
+	var player = null // UtilitiesFactory.findById(GameFactory.players, $stateParams.playerId);
+	// assigns playerId to highestRoller player_id
+	// returns player id of highest roller
+	$scope.rollForFirst = function(playerId){
+		player = UtilitiesFactory.findById(GameFactory.players, playerId);
+		console.log("player: " + player);
+		require(['../lib/chance.js'], function(Chance){
+			var chance = new Chance();
+//		var numberRolled = DiceFactory.roll(); // use when ready
+			var numberRolled = chance.integer({min:2, max:12});
+			if(numberRolled > highestRoller[0].roll){
+				highestRoller = [];
+				highestRoller.push({player: player, roll: numberRolled, show: true})
+				console.log("highest = true, numberRolled: " + numberRolled +
+										", highestRoller[0].player.name: " + highestRoller[0].player.name +
+										", .roll: " + highestRoller[0].roll);
+				return true;
+			}else {
+				console.log("highest = false, numberRolled: " + numberRolled +
+						", highestRoller.player.name: " + highestRoller[0].player.name +
+						", .roll: " + highestRoller[0].roll);
+				return false}			
+		});		
+	}
+	$scope.highRoller = highestRoller;
+/////////////////////////////////////////////////////////////////////
+/////////////////// end highest roller controls /////////////////////
+/////////////////////////////////////////////////////////////////////
+		
 });
