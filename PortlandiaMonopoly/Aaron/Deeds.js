6cf5c91245c5a2ad1e51c2797b39875f91883f4d
Aaron Campf <AaronCampf@Gmail.com>
2015-10-13 14:36:14 -0700

    Adding Classes

---
+++ PortlandiaMonopoly/Aaron/Deeds.js
@@ -0,0 +1,82 @@
+var deeds = [
+    //Go
+    new Deed("G0", 0, 0, 0, [], 0, ""),
+    //Mediterranean Avenue
+    new Deed("JAMA SOFTWARE", 60, 30, 50, [2, 10, 30, 90, 160], 1, "company"),
+    //Community Chest
+    new Deed("", 0, 0, 0, [], 0, ""),
+    //Baltic Avenue
+    new Deed("ALTSOURCE", 60, 30, 50, [4, 20, 60, 180, 320, 450], 1, "company"),
+    //Income Tax
+    new Deed("PORTLAND ART TAX", 0, 0, 0, [], 0, ""),
+    //Reading Railroad
+    new Deed("MAX", 200, 100, 0, [25, 50, 100, 200], 2, "train"),
+    //Oriental Avenue
+    new Deed("ZAPPROVED", 100, 50, 50, [6, 30, 90, 270, 400, 550], 3, "company"),
+    //Chance
+    new Deed("", 0, 0, 0, [], 0, ""),
+    //Vermont Avenue
+    new Deed("GLOBESHERPA", 100, 50, 50, [6, 30, 90, 270, 400, 550], 3, "company"),
+    //Connecticut Avenue
+    new Deed("THE DYRT", 120, 60, 50, [8, 40, 100, 300, 450, 600], 3, "company"),
+    //Jail
+    new Deed("PORTLAND SATURDAY MARKET", 0, 0, 0, [], 0, ""),
+    //St. Charles Place
+    new Deed("BRANDLIVE", 140, 70, 100, [10, 50, 150, 450, 625, 750], 4, "company"),
+    //Electric Company
+    new Deed("PORTLAND TRAILBLAZERS", 150, 75, 0, [], 5, ""),
+    //States Avenue
+    new Deed("THINKSHOUT", 140, 70, 100, [10, 50, 150, 450, 625, 750], 4, "company"),
+    //Virginia Avenue
+    new Deed("ACQUIA", 160, 80, 100, [12, 60, 180, 500, 700, 900], 4, "company"),
+    //Pennsylvania Railroad
+    new Deed("PORTLAND STREETCAR", 200, 100, 0, [25, 50, 100, 200], 2, "train"),
+    //St. James Place
+    new Deed("NOTION", 180, 90, 100, [14, 70, 200, 550, 750, 950], 6, "company"),
+    //Community Chest
+    new Deed("", 0, 0, 0, [], 0, ""),
+    //Tennessee Avenue
+    new Deed("ARUBA NETWORKS", 180, 90, 100, [14, 70, 200, 550, 750, 950], 6, "company"),
+    //New York
+    new Deed("RENTRAK", 200, 100, 100, [16, 80, 220, 600, 800, 1000], 6, "company"),
+    //Free Parking
+    new Deed("WASHINGTON PARK", 0, 0, 0, [], 0, ""),
+    //Kentucky Avenue
+    new Deed("RADAR", 220, 110, 150, [18, 90, 250, 700, 875, 1050], 7, "company"),
+    //Chance
+    new Deed("", 0, 0, 0, [], 0, ""),
+    //Indiana Avenue
+    new Deed("CHIRPIFY", 220, 110, 150, [18, 90, 250, 700, 875, 1050], 7, "company"),
+    //Illinois Avenue
+    new Deed("IOVATION", 240, 120, 150, [20, 100, 300, 750, 925, 1100], 7, "company"),
+    //B & O Railroad
+    new Deed("PORTLAND AERIAL TRAM", 200, 100, 0, [25, 50, 100, 200], 2, ""),
+    //Atlantic Avenue
+    new Deed("METAL TOAD", 260, 130, 150, [22, 110, 330, 800, 975, 1150], 8, "company"),
+    //Ventnor Avenue
+    new Deed("WEBTRENDS", 260, 130, 150, [22, 110, 330, 800, 975, 1150], 8, "company"),
+    //Water Works
+    new Deed("PORTLAND TIMBERS", 150, 75, 0, [], 5, ""),
+    //Marvin Gardens
+    new Deed("APPNEXUS", 280, 140, 150, [24, 120, 360, 850, 1025, 1200], 8, "company"),
+    //Go To Jail
+    new Deed("GO TO PORTLAND SATURDAY MARKET", 0, 0, 0, [], 0, ""),
+    //Pacific Avenue
+    new Deed("CROWDCOMPASS", 300, 150, 200, [26, 130, 390, 900, 1100, 1275], 9, "company"),
+    //North Carolina Avenue
+    new Deed("JIVE SOFTWARE", 300, 150, 200, [26, 130, 390, 900, 1100, 1275], 9, "company"),
+    //Community Chest
+    new Deed("", 0, 0, 0, [], 0, ""),
+    //Pennsylvania Avenue
+    new Deed("PUPPET LABS", 320, 160, 200, [28, 150, 450, 1000, 1200], 9, "company"),
+    //Short Line Railroad
+    new Deed("WASHINGTON PARK AND ZOO RAILWAY", 200, 100, 0, [25, 50, 100, 200], 2, ""),
+    //Chance
+    new Deed("", 0, 0, 0, [], 0, ""),
+    //Park Place
+    new Deed("NEW RELIC", 350, 175, 200, [35, 175, 500, 1100, 1300, 1500], 10, "company"),
+    //Luxury Tax
+    new Deed("VOODOO DOUGHNUTS", 0, 0, 0, [], 0, ""),
+    //Boardwalk
+    new Deed("FREE GEEK", 400, 200, 200, [50, 200, 600, 1400, 1700, 2000], 10, "company")
+];
